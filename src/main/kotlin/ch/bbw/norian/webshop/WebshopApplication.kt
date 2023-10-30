aa2d384ce28bf9e93081bf57ac9c9ba094a6e712
Ian Raschle <ian.raschle@ergon.ch>
2023-10-30 08:43:55 +0100

    initial commit

---
+++ src/main/kotlin/ch/bbw/norian/webshop/WebshopApplication.kt
@@ -0,0 +1,11 @@
+package ch.bbw.norian.webshop
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebshopApplication
+
+fun main(args: Array<String>) {
+    runApplication<WebshopApplication>(*args)
+}
---
+++ src/test/kotlin/ch/bbw/norian/webshop/WebshopApplicationTests.kt
@@ -0,0 +1,13 @@
+package ch.bbw.norian.webshop
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebshopApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
