4c69fccf60eb0ea4e25905a0f9487ac4a9f557ea
Norwin Schäfer <norwin270@gmail.com>
2023-11-06 11:10:18 +0100

    added angular components

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Product} from "./product";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'web';
+  title = 'PC Shop';
+  products: Product[] = [];
+
+  constructor() {
+    this.products.push({
+      id: 1,
+      title: "RTX 4090",
+      description: "Mega krasse Grafikkarte",
+      pictures: [],
+      price: 12.50,
+      category: "Grafikkarte",
+      remaining: 5, rating: 4.3
+    });
+    this.products.push({
+      id: 2,
+      title: "RTX 4080",
+      description: "Krasse Grafikkarte",
+      pictures: [],
+      price: 10.50,
+      category: "Grafikkarte",
+      remaining: 3, rating: 4.3
+    });
+  }
 }
--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -2,13 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { ProductComponent } from './product/product.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import {MatCardModule} from "@angular/material/card";
+import {NgOptimizedImage} from "@angular/common";
+import {MatButtonModule} from "@angular/material/button";
+import {MatGridListModule} from "@angular/material/grid-list";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingCartComponent,
+    ProductComponent,
+    ProductDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    NgOptimizedImage,
+    MatButtonModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
