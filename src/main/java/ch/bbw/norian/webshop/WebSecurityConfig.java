bcd7f99300f6ff7cacc4deae3f2e48313469d20a
Ian Raschle <ian.raschle@ergon.ch>
2023-12-11 08:49:35 +0100

    disabled csrf

--- src/main/java/ch/bbw/norian/webshop/WebSecurityConfig.java
+++ src/main/java/ch/bbw/norian/webshop/WebSecurityConfig.java
@@ -2,10 +2,14 @@ package ch.bbw.norian.webshop;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -15,5 +19,16 @@ public class WebSecurityConfig {
         InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
         manager.createUser(User.withDefaultPasswordEncoder().username("user").password("password").roles("USER").build());
         return manager;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(authorize -> authorize
+                        .anyRequest().authenticated()
+                ).csrf().disable()
+                .formLogin(withDefaults())
+                .httpBasic(withDefaults());
+        return http.build();
     }
 }
